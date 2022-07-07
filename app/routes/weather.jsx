5c3e49e7e7addc953b1eb8b7740f6630feef01c5
Joe Irvine <jirvine124@gmail.com>
2022-07-06 21:22:20 -0700

    weather page background image working correctly

--- app/routes/weather.jsx
+++ app/routes/weather.jsx
@@ -11,17 +11,19 @@ export function links() {
 export default function Weather() {
   // console.log(data)
   return (
-    <div className="weather">
-      <Outlet />
-      <Form id="form" action="/weather" method="Get">
-        <input
-          id="search"
-          type="text"
-          name="query"
-          placeholder="Search Weather"
-        />
-        <input className="submit" type="submit" value="Search" />
-      </Form>
+    <div className="weather-container">
+      <div className="weather">
+        <Outlet />
+        <Form id="form" action="/weather" method="Get">
+          <input
+            id="search"
+            type="text"
+            name="query"
+            placeholder="Search Weather"
+          />
+          <input className="submit" type="submit" value="Search" />
+        </Form>
+      </div>
     </div>
   );
 }
